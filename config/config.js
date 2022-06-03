472c6cc741df7b21a5520bdec08173fae92bbf0e
pitiwatMIKE <pitiwat.pr61@gmail.com>
2022-06-03 18:15:30 +0700

    change config.json to config.js

---
+++ config/config.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOSTNAME,
+    port: process.env.DEV_DB_PORT,
+    dialect: process.env.DEV_DB_DIALECT,
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: process.env.CI_DB_HOSTNAME,
+    port: process.env.CI_DB_PROT,
+    dialect: "mariadb",
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: process.env.PROD_DB_DIALECT,
+  },
+};
